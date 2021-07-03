bd7505bbd3c46bbd1d8cea374a49209f4e99e381
LeftyDark <rysmccar@ucsc.edu>
2021-07-02 17:31:52 -0700

    Started on collisions and did some debugging

--- src/scenes/menu.js
+++ src/scenes/menu.js
@@ -11,7 +11,7 @@ class Menu extends Phaser.Scene {
         this.load.audio('playBGMF', './assets/BGM/fastpiano_play.wav');
 
         // load image here
-        this.load.image('white_placeholder','./assets/Sprites/white.png');
+        this.load.image('white_placeholder','./assets/Sprites/white.jpg');
     }
     create() {
         this.sound.play('menuBGM');
@@ -19,12 +19,13 @@ class Menu extends Phaser.Scene {
             obstacleSpeed: 1
         }
         this.sound.play('sfx_select');
-        this.white - this.add.tileSprite(300, 300, 640, 480, 'white').setorigin(0,0);
-
+        this.add.image(0, 0, 'white_placeholder');
+        
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
     }
     update() {
         if(Phaser.Input.Keyboard.JustDown(keySPACE)) {
+            this.sound.stopAll();
             this.scene.start('playScene');
         }
         // this.scene.start('playScene');
--- src/scenes/play.js
+++ src/scenes/play.js
@@ -8,5 +8,17 @@ class Play extends Phaser.Scene {
         this.sound.play('playBGMF');
     }
     update() {}
-    // collision check function(s) here
+    // This collision function will run a specific function depending on 
+    //what two object are colliding
+    checkCollision(player, object, resultFunction) {
+        if(player.x < object.x + object.width &&
+            player.x + player.width > object.x &&
+            player.y < object.y + object.height &&
+            player.height + player.y > y)
+            { resultFunction};
+    }
+    monsterCollision() {
+        //returns the player to menu upon colliding with the monster
+        this.scene.play("menuScene");
+    }
 }
