1e67a2f77192285ff4bb085055d259ed6d747eda
LeftyDark <rysmccar@ucsc.edu>
2021-07-03 14:23:51 -0700

    Added ground to the playScene

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -3,9 +3,23 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
     init() {}
-    preload() {}
+    preload() {
+        this.load.image('placeholder_ground','./assets/Sprites/placeholder_ground.png');
+    }
+
     create() {
         this.sound.play('playBGMF');
+        this.MAX_VELOCITY = 500;
+        this.physics.world.gravity.y = 1000;
+
+        //creating ground
+        this.ground = this.add.group();
+        for( let i = 0; i < game.config.width; i += 20) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - 20, 'placeholder_ground').setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
     }
     update() {}
     // This collision function will run a specific function depending on 
