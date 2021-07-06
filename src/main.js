ddb819d20589400cdeecddde78645fb9856211af
LeftyDark <rysmccar@ucsc.edu>
2021-07-05 22:57:58 -0700

    Added names and creative tilt to main.js

--- src/main.js
+++ src/main.js
@@ -2,18 +2,24 @@
 
 Names:
     Masasteru Nakajima
-    (Insert Name Here)
-    (Insert Name Here)
+    Ryan McCarty
+    Nathanael Fonken
 
 Project Title:
     Endless Runner
 
-Date:
-    July 6, 2021
+Date Completed:
+    July 5, 2021
 
-Project Length:
-    ???
-
+Creative Tilt Justification: 
+-We added both ground and air obstacles to our runner, and also made them function differently, the obstacles
+on the ground pushed the player towars the can that makes them lose, whereas the air obstacles stop the 
+player's jump and put them in an uncontrolled fall while slowing them down, potentially forcing them to 
+land in a ground obstacle, pushing them and forcing them to jump again. In addition, our lose condition was
+different than most endless runners, it being colliding with a can on the left end of the screen rather than
+being hitting any obstacle or falling off the map.
+-Our visual style was hopefully unique in following a bean trying to escape from a can of beans attempting to 
+pack him, something that I imagine has rarely been done before as the scenario for an endless runner.
     
 ***************************/
 
