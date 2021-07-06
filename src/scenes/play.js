f659c575ccc439a5c7d26c588ac523e131e15d55
LeftyDark <rysmccar@ucsc.edu>
2021-07-05 22:44:38 -0700

    Added can and fixed collision issues

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -7,6 +7,7 @@ class Play extends Phaser.Scene {
         this.load.image('ground','./assets/Sprites/placeholder_ground.png');
         this.load.image('player', './assets/Sprites/noun_runningman_10.png');
         this.load.spritesheet('playerSprite', './assets/Sprites/beanman-running.png', {frameWidth: 64, frameHeight: 64, startFrame: 0, endFrame: 7});
+        this.load.spritesheet('canSprite', './assets/Sprites/can.png', {frameWidth: 248, frameHeight: 248, startFrame: 0, endFrame: 7});
         //running person by Kathleen Black from the Noun Project
         this.load.image('obstacle', './assets/Sprites/enemy_bean-export.png');
         this.load.image('monster', './assets/Sprites/placeholder_deathbox.png');
@@ -36,10 +37,10 @@ class Play extends Phaser.Scene {
             this.ground.add(groundTile);
         }
         //creating the different objects in scene. 
-        this.player = new Player(this, game.config.width/2, game.config.height-100, 'playerSprite');
+        this.player = new Player(this, game.config.width*0.8, game.config.height-100, 'playerSprite');
         this.player.setCollideWorldBounds(true);
         this.physics.add.collider(this.player, this.ground);
-        this.monster = this.physics.add.sprite(game.config.width-590, game.config.height-260, 'monster');
+        this.monster = this.physics.add.sprite(game.config.width-550, game.config.height-140, 'canSprite');
         this.monster.body.immovable = true;
         this.monster.body.allowGravity = false;
         this.physics.add.collider(this.monster, this.ground);
@@ -52,6 +53,13 @@ class Play extends Phaser.Scene {
             repeat: -1
         });
         this.player.anims.play('playerMove', 8, true);
+        this.canAni = this.anims.create({
+            key: 'canMove',
+            frames: this.anims.generateFrameNumbers('canSprite', {start: 0, end: 7, first: 0}),
+            frameRate: 8,
+            repeat: -1
+        });
+        this.monster.anims.play('canMove', 8, true);
 
         //creating lists of ground and air obstacles
         gObstacleList = []
@@ -78,33 +86,57 @@ class Play extends Phaser.Scene {
     update() {
         this.player.update();
         gObstacleList.forEach(obstacle => this.obstacleDelete(obstacle, this.monster));
-        aObstacleList.forEach(obstacle => this.obstacleDelete(obstacle, this.monster));
-        gObstacleList.forEach(obstacle => this.pushPlayer(this.player, obstacle));
+        aObstacleList.forEach(obstacle => this.aObstacleDelete(obstacle));
+        this.pushPlayerByObs(this.player, gObstacleList);
         aObstacleList.forEach(obstacle => this.dropPlayer(this.player, obstacle));
         this.gameOver(this.player, this.monster);
     }
     // This function will delete an obstacle when it collides with the monster at the edge of the screen.
     //It has a slight bug where if the two obstacles are both colliding at the same time, it will fail to delete 1 of them.
     obstacleDelete(object1, object2) {
-        if (object1.x < object2.x + object2.width && 
-            object1.x + object1.width > object2.x && 
-            object1.y < object2.y + object2.height &&
-            object1.height + object1.y > object2. y) {
+        if (object2.x > object1.x - (object1.width/2) - (object2.width/2) &&
+        object2.x < object1.x + (object1.width/2) + (object2.width/2) &&
+        object2.y > object1.y - (object1.height/2) -(object2.height/2) &&
+        object2.y < (object1.y + object1.height/2 + object2.height/2)) {
                 object1.destroy();
         } else {
             return false;
         }
     }
+    aObstacleDelete(object1) {
+        if (object1.x < game.config.width-640) { 
+            object1.destroy();    
+        }
+    }
+   
     gameOver(player, monster) {
         //returns the player to menu upon colliding with the monster
-        if (player.x < monster.x + monster.width && 
-            player.x + player.width > monster.x && 
-            player.y < monster.y + monster.height &&
-            player.height + player.y > monster. y) {
+        if (monster.x > player.x - (player.width/2) - (monster.width/2) &&
+        monster.x < player.x + (player.width/2) + (monster.width/2) &&
+        monster.y > player.y - (player.height/2) -(monster.height/2) &&
+        monster.y < (player.y + player.height/2 + monster.height/2)) {
                 this.sound.stopAll();
                 this.scene.start("menuScene");
         } else {
             return false;
+        }
+    }
+    pushPlayerByObs(player, obstacles) {
+        let hit = false;
+        for (let obstacle of obstacles) {
+            if (player.x < obstacle.x + obstacle.width && 
+                player.x + player.width > obstacle.x && 
+                player.y < obstacle.y + obstacle.height &&
+                player.height + player.y > obstacle. y) {
+                    hit = true;
+                    break;
+            }
+        }
+        if (hit) {
+            player.setVelocityX(-100);
+        }
+        else {
+            player.setVelocityX(0);
         }
     }
     pushPlayer(player, gObstacle) {
@@ -129,7 +161,7 @@ class Play extends Phaser.Scene {
     }
     CreateGroundObstacle() {
         //generates a new Ground Obstacle
-        this.gObstacle = new Obstacle(this, game.config.width*0.8, game.config.height-100, 'obstacle');
+        this.gObstacle = new Obstacle(this, game.config.width, game.config.height-100, 'obstacle');
         this.gObstacle.setVelocityX(this.gObstacle.moveSpeed);
         this.physics.add.collider(this.gObstacle, this.ground);
         gObstacleList.push(this.gObstacle);
